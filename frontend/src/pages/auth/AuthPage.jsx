1ad26672971bb863285ae0d4f8d9a1391c387dcd
MartaLourido <martalouri@gmail.com>
2021-04-17 10:30:08 +0200

    login and sign up working with design

--- frontend/src/pages/auth/AuthPage.jsx
+++ frontend/src/pages/auth/AuthPage.jsx
@@ -1,46 +1,120 @@
+// NPM Packages
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+// Project files
+import SignUp from "./SignUp";
+import Auth from "../../services/Auth";
+import Login from "./Login";
 
-export default function AuthPage({ onSubmit }) {
+// Project styiling from material ui
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="/">
+        TalkBench
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage:
+      "url(https://images.unsplash.com/photo-1564865878688-9a244444042a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function AuthPage(onSubmit) {
+  // Methods
+  async function login(loginData) {
+    const loginSuccess = await Auth.login(loginData);
+    if (!loginSuccess) {
+      alert("Invalid credentials");
+    }
+  }
+
+  const classes = useStyles();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <div className="card">
-      <div className="card-body">
-        <h4 className="card-title">Login</h4>
-        <div>
-          <div className="form-group">
-            <label>Email:</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <div className="AvatarWrap">
+            <img
+              width="70px"
+              height="auto"
+              className="img-responsive"
+              src="https://i.ibb.co/znyPmZf/ourpet.png"
+              alt="logo"
             />
+            {/* <Carousel/> */}
           </div>
-
-          <div className="form-group">
-            <label>Password:</label>
-            <input
-              type="password"
-              placeholder="Password"
-              className="form-control"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+          <Typography component="h1" variant="h5">
+            {/* Sign in */}
+          </Typography>
+          <div className="col-12  strong-shadow">
+            <Login onSubmit={login} />
+            
           </div>
-
-          <div className="form-group">
-            <button
-              className="btn btn-info"
-              onClick={() => onSubmit({ email, password })}
-            >
-              Login
-            </button>
-          </div>
+          
+          <Grid container>
+            <Grid item xs>
+            <div className="AvatarWrap">
+              <Link to="/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </div>
+            </Grid>
+          
+          </Grid>
+          <Box mt={5}>
+            <Copyright />
+          </Box>
         </div>
-      </div>
-    </div>
+      </Grid>
+    </Grid>
   );
 }
